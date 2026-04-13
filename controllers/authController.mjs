e75eb0a21d058ccc534635eb4d9774f434358b5f
Ashwin Bhanage <bhanageashwin28@gmail.com>
2026-04-13 22:53:17 +0530

    feat: add controllers for login and register users

---
+++ controllers/authController.mjs
@@ -0,0 +1,100 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // validation
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields required" });
+        }
+
+        // find user
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email],
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const user = result.rows[0];
+
+        // compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // generate token
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" },
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+            },
+        });
+    } catch (err) {
+        console.error("LOGIN ERROR:", err);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // validation
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields required" });
+        }
+
+        // check existing user
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email],
+        );
+
+        if (result.rowCount > 0) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // insert user
+        await pool.query(
+            "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+            [name, email, hashedPassword],
+        );
+
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (err) {
+        console.error("REGISTER ERROR:", err);
+        res.status(500).json({ message: err.message });
+    }
+};
